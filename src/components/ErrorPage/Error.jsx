4bbcff384f11bd760d2984f0742d8f89404f1572
sakib-git <sakibulhasan32002@gmail.com>
2025-12-06 17:44:35 +0600

    error page

--- src/components/ErrorPage/Error.jsx
+++ src/components/ErrorPage/Error.jsx
@@ -4,12 +4,13 @@ import { Link } from 'react-router';
 const Error = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-      <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
-      <p className="text-xl text-gray-700 mb-6">Page Not Found</p>
-      <Link to="/" className="text-blue-600 underline">
+      <h1 className="text-9xl font-bold text-red-500 mb-4 animate-bounce">404</h1>
+      <p className="text-4xl font-bold text-gray-700 mb-6">Page Not Found</p>
+      <Link to="/" className="bg-[#25408f] text-white btn">
         Go Back Home
       </Link>
     </div>
+
   );
 };
 
